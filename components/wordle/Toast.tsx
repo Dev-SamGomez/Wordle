b26af168c7044f6c08a9de7337e6c36d854fd23e
Samuel Gomez <sgomez@netmind.com.mx>
2026-02-19 15:01:29 -0700

    fix: animation toast

--- components/wordle/Toast.tsx
+++ components/wordle/Toast.tsx
@@ -1,15 +1,53 @@
 "use client";
 
+import { useEffect, useState } from "react";
+
 interface ToastProps {
-  message: string;
+  message: string | null;
 }
 
+const DURATION = 300;
+
 export function Toast({ message }: ToastProps) {
-  if (!message) return null;
+  const [render, setRender] = useState(false);
+  const [show, setShow] = useState(false);
+  const [internalMessage, setInternalMessage] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (message) {
+      setInternalMessage(message);
+      setRender(true);
+
+      setTimeout(() => {
+        setShow(true);
+      }, 10);
+    } else if (render) {
+      setShow(false);
+
+      const timeout = setTimeout(() => {
+        setRender(false);
+        setInternalMessage(null);
+      }, DURATION);
+
+      return () => clearTimeout(timeout);
+    }
+  }, [message]);
+
+  if (!render) return null;
 
   return (
-    <div className="absolute top-16 left-1/2 -translate-x-1/2 z-50 bg-white text-[#121213] font-bold text-sm px-4 py-3 rounded shadow-lg animate-fade-in">
-      {message}
+    <div
+      className={`
+        absolute top-16 left-1/2 -translate-x-1/2 z-50
+        bg-white text-[#121213] font-bold text-sm
+        px-4 py-3 rounded shadow-lg
+        transition-all duration-300 ease-in-out
+        ${show
+          ? "opacity-100 translate-y-0"
+          : "opacity-0 -translate-y-4"}
+      `}
+    >
+      {internalMessage}
     </div>
   );
 }
