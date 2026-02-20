575f2216708f89950b2dbe52904b6f9ab1231f89
Samuel Gomez <sgomez@netmind.com.mx>
2026-02-19 17:38:40 -0700

    feat: add ñ letter

--- components/wordle/Keyboard.tsx
+++ components/wordle/Keyboard.tsx
@@ -10,7 +10,7 @@ interface KeyboardProps {
 
 const ROWS = [
   ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
-  ["A", "S", "D", "F", "G", "H", "J", "K", "L"],
+  ["A", "S", "D", "F", "G", "H", "J", "K", "L", "Ñ"],
   ["ENTER", "Z", "X", "C", "V", "B", "N", "M", "BACKSPACE"],
 ];
 
